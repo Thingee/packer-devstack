59c21145cf9a3ea291ba44aa53a8561f6c247d35
Mike Perez <thingee@gmail.com>
2015-02-05 16:11:22 -0800

    Just install git and skip upgrading packages.
    Devstack takes care of these package dependencies already.

--- scripts/root_setup.sh
+++ scripts/root_setup.sh
@@ -1,11 +1,7 @@
 #!/bin/sh -x
 
-# Updating and Upgrading dependencies
 apt-get update -y -qq > /dev/null
-apt-get upgrade -y -qq > /dev/null
-
-# Install necessary dependencies
-apt-get -y -q install linux-headers-$(uname -r) build-essential git screen python-pip curl python-dev python-setuptools libmysqlclient-dev libpq-dev libxslt1-dev libxml2-dev libffi-dev curl wget git vim
+apt-get install -y -q git
 
 # Setup sudo to allow no-password sudo for "admin"
 groupadd -r admin
